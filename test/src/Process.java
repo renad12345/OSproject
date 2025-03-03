d04cb795f9c8a01bc000c829e4209113b34322ff
marammsh <162722292+marammsh@users.noreply.github.com>
2025-03-03 22:44:41 +0300

    adding new variable

--- test/src/Process.java
+++ test/src/Process.java
@@ -2,18 +2,39 @@ class Process {
     private int id;
     private int arrivalTime;
     private int burstTime;
+private int completionTime;
+private int originalBurstTime;
 
-    public Process(int id, int arrivalTime, int burstTime) {
+    public Process(int id, int arrivalTime, int burstTime, int completionTime, int originalBurstTime) {
         this.id = id;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
+        this.completionTime = completionTime;
+        this.originalBurstTime = originalBurstTime;
     }
 
+ public Process(int id, int arrivalTime, int burstTime) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.originalBurstTime = burstTime;  
+    }
     public Process(Process process) {
         id = process.getID();
         burstTime = process.getBurstTime();
         arrivalTime = process.getArrivalTime();
+        originalBurstTime = process.getOriginalBurstTime(); 
     }
+public int getCompletionTime() {
+    return completionTime;
+}
+
+public void setCompletionTime(int completionTime) {
+    this.completionTime = completionTime;
+}
+public int getOriginalBurstTime() {
+    return originalBurstTime;
+}
 
 
     @Override
@@ -34,5 +55,14 @@ class Process {
 
     public void setBurstTime(int burstTime) {
         this.burstTime = burstTime;
+       
     }
+   // public int getOriginalBurstTime() {
+   // return originalBurstTime;
+//}
+
+public void setOriginalBurstTime(int originalBurstTime) {
+    this.originalBurstTime = originalBurstTime;
+}
+
 }
